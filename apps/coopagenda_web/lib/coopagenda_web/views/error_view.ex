146d9444169e0082d386d1c33489440f6e76e1ff
Pehuén <pehuen.rodriguez@nayra.coop>
2020-04-20 00:56:36 -0300

    Add base error views

--- apps/coopagenda_web/lib/coopagenda_web/views/error_view.ex
+++ apps/coopagenda_web/lib/coopagenda_web/views/error_view.ex
@@ -1,16 +1,26 @@
 defmodule CoopagendaWeb.ErrorView do
   use CoopagendaWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
+  def render("401.json", _assigns) do
+    %{title: "Unauthorized", code: 401}
+  end
 
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  def render("403.json", _assigns) do
+    %{title: "Forbidden", code: 403}
+  end
+
+  def render("404.json", _assigns) do
+    %{title: "Page not found", code: 404}
+  end
+
+  def render("422.json", _assigns) do
+    %{title: "Unprocessable entity", code: 422}
+  end
+
+  def render("500.json", _assigns) do
+    %{title: "Internal Server Error", code: 500}
+  end
+
+  def template_not_found(_template, _assigns) do
   end
 end
