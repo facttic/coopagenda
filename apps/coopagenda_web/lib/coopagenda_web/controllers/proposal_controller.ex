d4036b0636739a644965644fbdd980e1428925c2
Pehuén <pehuen.rodriguez@nayra.coop>
2020-04-20 00:54:42 -0300

    Fetch slot before proposal creation

--- apps/coopagenda_web/lib/coopagenda_web/controllers/proposal_controller.ex
+++ apps/coopagenda_web/lib/coopagenda_web/controllers/proposal_controller.ex
@@ -12,10 +12,11 @@ defmodule CoopagendaWeb.ProposalController do
   end
 
   def create(conn, %{"proposal" => proposal_params}) do
-    with {:ok, %Proposal{} = proposal} <- Agenda.create_proposal(proposal_params) do
+    slot = Agenda.get_slot!(proposal_params.slot_id)
+    with {:ok, %Proposal{} = proposal} <- Agenda.create_proposal(slot, proposal_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.proposal_path(conn, :show, proposal))
+      |> put_resp_header("location", Routes.slot_proposal_path(conn, :show, proposal))
       |> render("show.json", proposal: proposal)
     end
   end
