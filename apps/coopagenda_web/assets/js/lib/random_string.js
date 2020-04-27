e32c19d0eb0d036683c09280bf9edd0f6f431c03
Pehuén <pehuen.rodriguez@nayra.coop>
2020-04-27 15:50:03 -0300

    Delint

--- apps/coopagenda_web/assets/js/lib/random_string.js
+++ apps/coopagenda_web/assets/js/lib/random_string.js
@@ -1,10 +1,10 @@
 export const makeid = (length) => {
-  var result = "";
-  var characters =
+  let result = "";
+  let characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
+};
